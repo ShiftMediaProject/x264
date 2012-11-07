glei/x264-devel

Fix ALIGNED_ARRAY_EMU macros on ICL ICL's preprocessor doesn't handle it correctly. This fix is similar to libav's fix in 0db2d9.

common/osdep.h
@@ -108,8 +108,10 @@
     ALIGNED_16( type name sub1 __VA_ARGS__ )
 #endif
 
-#define ALIGNED_ARRAY_32( ... ) ALIGNED_ARRAY_EMU( 31, __VA_ARGS__ )
-#define ALIGNED_ARRAY_64( ... ) ALIGNED_ARRAY_EMU( 63, __VA_ARGS__ )
+#define E(x) x
+
+#define ALIGNED_ARRAY_32( ... ) E( ALIGNED_ARRAY_EMU( 31, __VA_ARGS__ ) )
+#define ALIGNED_ARRAY_64( ... ) E( ALIGNED_ARRAY_EMU( 63, __VA_ARGS__ ) )
 
 #define UNINIT(x) x=x
 

ShiftMediaProject/x264

common/osdep.h
@@ -108,8 +108,10 @@
     ALIGNED_16( type name sub1 __VA_ARGS__ )
 #endif
 
-#define ALIGNED_ARRAY_32( ... ) ALIGNED_ARRAY_EMU( 31, __VA_ARGS__ )
-#define ALIGNED_ARRAY_64( ... ) ALIGNED_ARRAY_EMU( 63, __VA_ARGS__ )
+#define EXPAND(x) x
+
+#define ALIGNED_ARRAY_32( ... ) EXPAND( ALIGNED_ARRAY_EMU( 31, __VA_ARGS__ ) )
+#define ALIGNED_ARRAY_64( ... ) EXPAND( ALIGNED_ARRAY_EMU( 63, __VA_ARGS__ ) )
 
 #define UNINIT(x) x=x
 