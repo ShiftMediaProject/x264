8477d9a6b6cdbc1ec2b9aa2ba4458a7b4d6ee44e
Matt Oliver <protogonoi@gmail.com>
2014-03-07 17:13:30 +1100

    Update x264 version.h

--- SMC/x264_config.h
+++ SMC/x264_config.h
@@ -2,7 +2,7 @@
 #define X264_GPL           1
 #define X264_INTERLACED    1
 #define X264_CHROMA_FORMAT 0
-#define X264_REV 2371
+#define X264_REV 2422
 #define X264_REV_DIFF 0
-#define X264_VERSION " r2371M SMC version"
-#define X264_POINTVER "0.138.2371M SMC version"
+#define X264_VERSION " r2422"
+#define X264_POINTVER "0.142.2422"
