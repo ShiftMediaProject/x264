da949a70750f6719ee9b63cc5b6c27c88a6e08b0
Matt Oliver <protogonoi@gmail.com>
2020-04-15 03:04:04 +1000

    project: Update for recent upstream merge.

--- SMP/x264_config.h
+++ SMP/x264_config.h
@@ -2,7 +2,7 @@
 #define X264_GPL           1
 #define X264_INTERLACED    1
 #define X264_CHROMA_FORMAT 0
-#define X264_REV 2991
+#define X264_REV 3000
 #define X264_REV_DIFF 0
-#define X264_VERSION " r2991"
-#define X264_POINTVER "0.159.2991"
+#define X264_VERSION " r3000"
+#define X264_POINTVER "0.160.3000"
