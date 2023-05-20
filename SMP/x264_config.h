067aa1efa30148bc3141aeba50ed1f81e414fede
Matt Oliver <protogonoi@gmail.com>
2023-05-21 01:59:03 +1000

    project: Update for recent merge.

--- SMP/x264_config.h
+++ SMP/x264_config.h
@@ -2,7 +2,7 @@
 #define X264_GPL           1
 #define X264_INTERLACED    1
 #define X264_CHROMA_FORMAT 0
-#define X264_REV 3106
+#define X264_REV 3107
 #define X264_REV_DIFF 0
-#define X264_VERSION " r3106"
-#define X264_POINTVER "0.164.3106"
+#define X264_VERSION " r3107"
+#define X264_POINTVER "0.164.3107"
